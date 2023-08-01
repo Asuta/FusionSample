9834c203ff196bf018457d07ffb3d055b90b953b
YDS！ <1023571809@qq.com>
2023-08-01 15:07:10 +0800

    应该是做好了

--- Assets/YouYouTest/Scripts/player/WeaponFather.cs
+++ Assets/YouYouTest/Scripts/player/WeaponFather.cs
@@ -9,8 +9,10 @@ public class WeaponFather : MonoBehaviour
     public bool isIgnore = true;
     public Transform[] ignoreObjects;
     private List<Rigidbody> ignoreRigids;
+    private List<Collider> ignoreColliders;
     public Transform[] ignoreSelfObjects;
     private List<Rigidbody> ignoreSelfRigids;
+    private List<Collider> ignoreSelfColliders;
     //----------------------test----------------------
     public float numberofIgnoreRigids;
     public float numberofIgnoreSelfRigids;
@@ -34,11 +36,19 @@ public class WeaponFather : MonoBehaviour
     private void SetIgnore(bool v)
     {
 
-        for (int i = 0; i < numberofIgnoreRigids; i++)
+        // for (int i = 0; i < numberofIgnoreRigids; i++)
+        // {
+        //     for (int j = 0; j < numberofIgnoreSelfRigids; j++)
+        //     {
+        //         Physics.IgnoreCollision(ignoreRigids[i].GetComponent<Collider>(), ignoreSelfRigids[j].GetComponent<Collider>(), v);
+        //     }
+        // }
+
+        for (int i = 0; i < ignoreColliders.Count; i++)
         {
-            for (int j = 0; j < numberofIgnoreSelfRigids; j++)
+            for (int j = 0; j < ignoreSelfColliders.Count; j++)
             {
-                Physics.IgnoreCollision(ignoreRigids[i].GetComponent<Collider>(), ignoreSelfRigids[j].GetComponent<Collider>(), v);
+                Physics.IgnoreCollision(ignoreColliders[i], ignoreSelfColliders[j], v);
             }
         }
 
@@ -47,12 +57,20 @@ public class WeaponFather : MonoBehaviour
     public void SetIgnoreRigids()
     {
         ignoreRigids = new List<Rigidbody>();
+        ignoreColliders = new List<Collider>();
         for (int i = 0; i < ignoreObjects.Length; i++)
         {
             Rigidbody[] rigids = ignoreObjects[i].GetComponentsInChildren<Rigidbody>();
             foreach (Rigidbody rigid in rigids)
             {
                 ignoreRigids.Add(rigid);
+            }
+
+            Collider[] colliders = ignoreObjects[i].GetComponentsInChildren<Collider>();
+            Debug.Log("colliders.Length:" + colliders.Length);
+            foreach (Collider collider in colliders)
+            {
+                ignoreColliders.Add(collider);
             }
         }
         numberofIgnoreRigids = ignoreRigids.Count;
@@ -61,12 +79,19 @@ public class WeaponFather : MonoBehaviour
     public void SetSelfIgnoreRigids()
     {
         ignoreSelfRigids = new List<Rigidbody>();
+        ignoreSelfColliders = new List<Collider>();
         for (int i = 0; i < ignoreSelfObjects.Length; i++)
         {
             Rigidbody[] rigids = ignoreSelfObjects[i].GetComponentsInChildren<Rigidbody>();
             foreach (Rigidbody rigid in rigids)
             {
                 ignoreSelfRigids.Add(rigid);
+            }
+
+            Collider[] colliders = ignoreSelfObjects[i].GetComponentsInChildren<Collider>();
+            foreach (Collider collider in colliders)
+            {
+                ignoreSelfColliders.Add(collider);
             }
         }
         numberofIgnoreSelfRigids = ignoreSelfRigids.Count;
