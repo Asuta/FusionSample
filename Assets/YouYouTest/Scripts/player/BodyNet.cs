f69a4976d6c0af19467cb04f07f6df979ba0efef
YDS！ <1023571809@qq.com>
2023-08-15 18:10:56 +0800

    加上了joint限制

--- Assets/YouYouTest/Scripts/player/BodyNet.cs
+++ Assets/YouYouTest/Scripts/player/BodyNet.cs
@@ -7,11 +7,25 @@ public class BodyNet : NetworkBehaviour
 {
     public Transform target;
     private Transform selfT;
+    public ConfigurableJoint confiJoint;
     // Start is called before the first frame update
     void Start()
     {
         selfT = transform;
+
+
+        if (HasStateAuthority == false)
+        {
+            //lock the xyz move of the joint
+            confiJoint.xMotion = ConfigurableJointMotion.Locked;
+            confiJoint.yMotion = ConfigurableJointMotion.Locked;
+            confiJoint.zMotion = ConfigurableJointMotion.Locked;
+        }
+
     }
+
+
+
 
 
     // Update is called once per frame
