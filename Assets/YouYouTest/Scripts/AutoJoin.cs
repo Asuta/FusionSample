355db4c5f54737827caf8f34f49d1bddf8301875
YDS！ <1023571809@qq.com>
2023-08-17 17:12:34 +0800

    碰撞测试

--- Assets/YouYouTest/Scripts/AutoJoin.cs
+++ Assets/YouYouTest/Scripts/AutoJoin.cs
@@ -15,6 +15,17 @@ public class AutoStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+        //start a coroutine to wait for the server to start
+        StartCoroutine(WaitForServer());
+    }
+
+ 
+    IEnumerator WaitForServer()
+    {
+        //wait for the server to start
+        yield return new WaitForSeconds(1f);
+        //start the game
         debugStart.StartSharedClient();
     }
 
