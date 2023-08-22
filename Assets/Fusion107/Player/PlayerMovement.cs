b50051b3f537a8edd483a12a2b20e95e45d8b5f0
YDS！ <1023571809@qq.com>
2023-08-22 10:36:47 +0800

    obj

--- Assets/Fusion107/Player/PlayerMovement.cs
+++ Assets/Fusion107/Player/PlayerMovement.cs
@@ -178,7 +178,7 @@ namespace Fusion107
             //PlayerSpeed -= damage;
             // log the name of the object
             Debug.LogError("damage name ====" + damageID);
-            Debug.LogError("damage  hash name ====" + damageObj.GetHashCode());
+            Debug.LogError("damage  hash name ====" + damageObj.name);
         }
 
         private void GrabSomething(Transform HandGrabT, Rigidbody HandRb)
